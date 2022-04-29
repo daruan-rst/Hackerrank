d948d18114cab783caa69685c1c7167b95efc961
daruan <daruan.rst@gmail.com>
2022-04-28 22:17:22 -0300

    Java loops

---
+++ src/com/hackerrank/JavaLoopsII/Solution.java
@@ -0,0 +1,26 @@
+package com.hackerrank.JavaLoopsII;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int s;
+        int q = sc.nextInt();
+        for (int i = 0; i < q; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
+            s = a;
+            for (int j = 0 ; j<n ; j++){
+                s = s + ((int) Math.pow(2, j))*b;
+                System.out.printf("%d ", s);
+            }
+            System.out.print("\n");
+        }
+        sc.close();
+    }
+}
