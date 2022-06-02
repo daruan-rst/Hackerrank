a254a520e3c334d5a50e718638bc212652e84cf4
daruan <daruan.rst@gmail.com>
2022-06-01 22:57:32 -0300

    StaticInitializerBlock

---
+++ src/com/hackerrank/JavaStaticInitializerBlock/Solution.java
@@ -0,0 +1,26 @@
+package com.hackerrank.JavaStaticInitializerBlock;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    private final static Scanner SC = new Scanner(System.in);
+    private static int h;
+    private static int b;
+
+
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        b = SC.nextInt();
+        h = SC.nextInt();
+        if (b <= 0 || h <= 0) {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        } else {
+            System.out.println(b*h);
+        }
+        SC.close();
+
+    }
+}
