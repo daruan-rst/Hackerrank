2ad3467a99823412427b07614074bc22f63efb64
daruan <daruan.rst@gmail.com>
2022-10-03 01:27:13 -0300

    Java End of File

---
+++ src/com/hackerrank/JavaEndOfFile/Solution.java
@@ -0,0 +1,18 @@
+    package com.hackerrank.JavaEndOfFile;
+
+    import java.io.*;
+    import java.util.*;
+
+
+    public class Solution {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int i = 1;
+            while (sc.hasNext()) {
+                System.out.println(i++ + " " + sc.nextLine());
+            }
+
+        }
+    }
+
+
