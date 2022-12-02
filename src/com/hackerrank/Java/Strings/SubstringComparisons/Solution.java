03240e1f8c120a3d829a3a2321ce8ca4b94afce6
daruan-rst <daruan.rst@gmail.com>
2022-12-01 21:45:38 -0300

    Java String: Substring Comparison

--- src/com/hackerrank/Java/Strings/SubstringComparisons/Solution.java
+++ src/com/hackerrank/Java/Strings/SubstringComparisons/Solution.java
@@ -3,29 +3,21 @@ package com.hackerrank.Java.Strings.SubstringComparisons;
 import java.util.Scanner;
 
 public class Solution {
-
     public static String getSmallestAndLargest(String s, int k) {
         String smallest = "";
         String largest = "";
-        String current = "";
         // Complete the function
         // 'smallest' must be the lexicographically smallest substring of length 'k'
         // 'largest' must be the lexicographically largest substring of length 'k'
-        for (int i = 0; i<s.length() ; i+=k){
-            int start = s.length()-i<k ? s.length()-k : i;
-            int end = Math.min(s.length(), (i+k));
-            current = s.substring(start, end);
-            if (i == 0){
-                smallest = current;
-            }else if (current.compareTo(smallest) < 0){
-                smallest = current;
-            }
-            if (current.compareTo(largest) > 0){
-                largest = current;
-            }
+
+        StringBuilder b = new StringBuilder(s);
+        String subString = "";
+        smallest = b.substring(0,k);
+        for (int i = 0; i + k -1 < s.length(); i++) {
+            subString = b.substring(i, i + k);
+            largest = subString.compareTo(largest) > 0 ? subString :  largest;
+            smallest = subString.compareTo(smallest) < 0 ? subString : smallest;
         }
-
-
 
         return smallest + "\n" + largest;
     }
