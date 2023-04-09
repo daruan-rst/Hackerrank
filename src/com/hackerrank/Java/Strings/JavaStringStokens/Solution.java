90d18f224211f6911fbcd7b63aa0a8d99a6cc251
daruan-rst <daruan.rst@gmail.com>
2023-04-09 19:27:54 -0300

    Java String Tokens

--- src/com/hackerrank/Java/Strings/JavaStringStokens/Solution.java
+++ src/com/hackerrank/Java/Strings/JavaStringStokens/Solution.java
@@ -8,7 +8,6 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
-//        List<String> tokens = Arrays.stream(s.split("[^\\w]")).collect(Collectors.toList());
         List<String> tokens = Arrays.stream(s.split("[\\s!,?._'@\\e\\n\\r]")).collect(Collectors.toList());
         tokens = tokens.stream().filter(st->!st.equals("")).collect(Collectors.toList());
         System.out.println(tokens.size());
