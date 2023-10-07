99e754fd8af4c9cb7c55358b2ad6aae1a55971ea
daruan-rst <daruan.rst@gmail.com>
2023-10-06 21:48:48 -0300

    InsertionSortPart1

---
+++ src/com/hackerrank/Kotlin/ProblemSolving/Sorting/InsertionSortPart1.kt
@@ -0,0 +1,35 @@
+package com.hackerrank.Kotlin.ProblemSolving.Sorting
+
+/*
+ * Complete the 'insertionSort1' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY arr
+ */
+
+fun insertionSort1(n: Int, arr: Array<Int>): Unit {
+    val unsortedElement = arr[n - 1]
+    var i = n - 2
+
+    while (i >= 0 && arr[i] > unsortedElement) {
+        arr[i + 1] = arr[i]
+        println(arr.joinToString(" "))
+        i--
+    }
+
+    arr[i + 1] = unsortedElement
+    println(arr.joinToString(" "))
+}
+
+fun printArray(arr: Array<Int>){
+    arr.forEach {a -> print("$a ") }
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val arr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    insertionSort1(n, arr)
+}
