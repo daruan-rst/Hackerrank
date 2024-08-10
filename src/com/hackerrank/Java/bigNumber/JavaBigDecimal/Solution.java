f20b834ae30a18a9028e103e6715887bb166cd93
Daruan <daruan.rst@gmail.com>
2024-08-10 11:28:41 -0300

    Java BigDecimal

--- src/com/hackerrank/Java/bigNumber/JavaBigDecimal/Solution.java
+++ src/com/hackerrank/Java/bigNumber/JavaBigDecimal/Solution.java
@@ -1,55 +1,30 @@
 package com.hackerrank.Java.bigNumber.JavaBigDecimal;
 
 import java.math.BigDecimal;
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.Scanner;
 
-class Solution{
+class Solution {
+    public static void main(String[] args) {
 
-    public static void main(String []args){
-        //Input
-        Scanner sc= new Scanner(System.in);
-        int n=sc.nextInt();
-        String []s=new String[n];
-        for(int i=0;i<n;i++){
-            s[i]=sc.next();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] s = new String[n];
+        for (int i = 0; i < n; i++) {
+            s[i] = sc.next();
         }
         sc.close();
 
-        //Write your code here
 
-        Function<String, BigDecimal> convert = (st) -> new BigDecimal(st).setScale(st.split(".")[1].length());
+        Arrays.sort(s, (a, b) -> {
+            BigDecimal bd1 = new BigDecimal(a);
+            BigDecimal bd2 = new BigDecimal(b);
+            return bd2.compareTo(bd1);
+        });
 
-        List<BigDecimal> bigDecimal_s = Arrays.stream(s).map(convert).collect(Collectors.toList());
 
-        List<String> sList = rearrange(s);
-
-        //Output
-        for(int i=0;i<n;i++)
-        {
-            System.out.println(sList.get(i));
+        for (String str : s) {
+            System.out.println(str);
         }
     }
-
-    private static List<String> rearrange(String[] s){
-        List<BigDecimal> sArr = Arrays.stream(s).map(BigDecimal::new).collect(Collectors.toList());
-        List<Integer> scale = sArr.stream().map(BigDecimal::scale).toList();
-
-        Comparator<BigDecimal> isDescAndScale = new Comparator<BigDecimal>() {
-            @Override
-            public int compare(BigDecimal o1, BigDecimal o2) {
-                boolean isDesc =  o1.compareTo(o2) < 0;
-                
-                return 0;
-            }
-        };
-
-        sArr.sort(Comparator.reverseOrder()
-        );
-        return sArr.stream().map(BigDecimal::toString).toList();
-
-    }
-
 }
