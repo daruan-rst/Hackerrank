7bfadb219d359aed72dc313a81c606d14a3845e4
Daruan <daruan.rst@gmail.com>
2024-08-17 18:35:33 -0300

    Java SubArray

--- src/com/hackerrank/Java/DataStructures/JavaSubarray/Solution.java
+++ src/com/hackerrank/Java/DataStructures/JavaSubarray/Solution.java
@@ -3,9 +3,7 @@ package com.hackerrank.Java.DataStructures.JavaSubarray;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
@@ -17,9 +15,10 @@ public class Solution {
 
         List<Integer> a = new ArrayList<>();
 
+        String[] inputNumbers = bufferedReader.readLine().trim().split(" ");
 
         for (int i = 0; i < n; i++) {
-            a.add(Integer.parseInt(bufferedReader.readLine().trim()));
+            a.add(Integer.parseInt(inputNumbers[i]));
         }
 
         bufferedReader.close();
@@ -27,7 +26,6 @@ public class Solution {
         int negativeArrays = 0;
         List<Integer> currentList;
 
-        // 1 2 3 4 5 6
         for(int i = 0 ; i < a.size() ; i++){
             for (int j = 0 ; j < a.size() ; j++ ){
                 currentList = List.copyOf(a.subList(j,j));
@@ -41,6 +39,6 @@ public class Solution {
     }
 
     private static boolean isNegativeSum(List<Integer> arr){
-        return arr.stream().reduce((a,b) -> a = a + b).get() < 1;
+        return arr.stream().reduce(0, Integer::sum) < 0;
     }
 }
