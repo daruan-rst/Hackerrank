ace69fc31ff45612c5160efc2f0b44847a37bee5
Daruan <daruan.rst@gmail.com>
2024-08-27 23:49:25 -0300

    Java 1D Array Part2

--- src/com/hackerrank/Java/DataStructures/Java1DArrayPart2/Solution.java
+++ src/com/hackerrank/Java/DataStructures/Java1DArrayPart2/Solution.java
@@ -5,19 +5,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Solution {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        int q = Integer.parseInt(bufferedReader.readLine().trim());
-        Map<String[], String[]> allGames = new HashMap<>();
-
-        for (int i = 0; i < q; i++) {
-            String[] n_leap = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-            String[] game = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-            allGames.put(n_leap, game);
+    public static boolean canWin(int leap, int[] game) {
+        int counter = 0;
+        for (int i = 1 ; i <= game.length - 1 ; i++){
+            if (game[i-1] == 0) counter --;
+            if (game[i-1] == 0) counter --;
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int q = scan.nextInt();
+        while (q-- > 0) {
+            int n = scan.nextInt();
+            int leap = scan.nextInt();
+
+            int[] game = new int[n];
+            for (int i = 0; i < n; i++) {
+                game[i] = scan.nextInt();
+            }
+
+            System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
+        }
+        scan.close();
     }
 }
