bfe54b501160f7690cf4983344d573f936bbb2f5
Daruan <daruan.rst@gmail.com>
2024-08-28 23:52:21 -0300

    Java 1D Array Part2

--- src/com/hackerrank/Java/DataStructures/Java1DArrayPart2/Solution.java
+++ src/com/hackerrank/Java/DataStructures/Java1DArrayPart2/Solution.java
@@ -1,20 +1,32 @@
 package com.hackerrank.Java.DataStructures.Java1DArrayPart2;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Solution {
 
     public static boolean canWin(int leap, int[] game) {
         int counter = 0;
-        for (int i = 1 ; i <= game.length - 1 ; i++){
-            if (game[i-1] == 0) counter --;
-            if (game[i-1] == 0) counter --;
+        for (int i = 0 ; i <= game.length; i++){
+            if (i + leap >= game.length){
+                counter = game.length - 1;
+                break;
+            }
+            if (i + leap < game.length){
+                if (game[i+leap] == 0){
+                    counter = i+leap;
+                }
+            }else if (i + 1 < game.length){
+                if (game[i+1] == 0){
+                    counter = i+1;
+                }
+            } else if (i > 0){
+                if (game[i-1] == 0){
+                    counter = i-1;
+                }
+            }
         }
+
+        return counter == game.length - 1;
     }
 
     public static void main(String[] args) {
